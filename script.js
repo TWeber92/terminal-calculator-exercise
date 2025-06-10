8ce305d368603b0c593953d530c88001bb0ff680
TWeber92 <tadweber92@gmail.com>
2025-06-10 13:31:18 -0600

    Removed node_module file

--- script.js
+++ script.js
@@ -1,54 +1,41 @@
 var readlineSync = require("readline-sync");
 
+const operators = {
+  "*": (a, b) => a * b,
+  "/": (a, b) => a / b,
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "%": (a, b) => a % b,
+};
+
 const terminalcalculation = () => {
-  const mathOper = getOperator();
-  const firstNum = getNumber("Please enter the first number: ");
-  const secondNum = getNumber("Please enter the second number: ");
-  const result = getResult(firstNum, secondNum, mathOper);
-  if (result === null) {
-    return terminalcalculation();
-  }
-  console.log(`The result is: ${result}`);
+  do {
+    const mathOper = getOperator();
+    const firstNum = getNumber("first");
+    let secondNum = getNumber("second");
+    while ((mathOper === "/" || mathOper === "%") && secondNum === 0) {
+      console.log("Cannot divide by zero. Please try again.");
+      secondNum = getNumber("second");
+    }
+    const result = getResult(firstNum, secondNum, mathOper);
+    console.log(`The result is: ${result}`);
+  } while (
+    readlineSync.keyInYNStrict("Do you want to perform another calculation? ")
+  );
+  console.log("Goodbye!");
 };
 function getOperator() {
-  const operators = ["*", "/", "+", "-", "%"];
-  while (true) {
-    const mathOper = readlineSync.question(
-      "What operation would you like to perform? "
-    );
-    if (operators.includes(mathOper)) {
-      return mathOper;
-    }
-    console.log("That is not a valid operation");
-  }
+  return readlineSync.question("What operation would you like to perform? ", {
+    limit: Object.keys(operators),
+    limitMessage: "That is not a valid operation",
+  });
 }
-function getNumber(prompt) {
-  while (true) {
-    const input = readlineSync.question(prompt);
-    const num = Number(input);
-    if (!isNaN(num)) {
-      return num;
-    }
-    console.log("This is not a number");
-  }
+function getNumber(order) {
+  return readlineSync.questionInt(`Please enter the ${order} number: `, {
+    limitMessage: "This is not a number",
+  });
 }
 function getResult(num1, num2, operator) {
-  if (operator === "-") return num1 - num2;
-  if (operator === "+") return num1 + num2;
-  if (operator === "*") return num1 * num2;
-  if (operator === "/") {
-    if (num2 === 0) {
-      console.log("Can not divide by zero.");
-      return null;
-    }
-    return num1 / num2;
-  }
-  if (operator === "%") {
-    if (num2 === 0) {
-      console.log("Can not divide by zero.");
-      return null;
-    }
-    return num1 % num2;
-  }
+  return operators[operator](num1, num2);
 }
 terminalcalculation();
